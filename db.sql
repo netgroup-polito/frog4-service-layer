f89c8fd32ed6ab554d0d308eaf25b74b16b330a7
Gabriele Castellano <gabry.c92x@gmail.com>
2016-03-12 19:34:01 +0100

    Added to db.sql some missing tables

--- db.sql
+++ db.sql
@@ -20,17 +20,76 @@ SET time_zone = "+00:00";
 -- Table structure for table `node`
 --
 
-CREATE TABLE `node` (
+CREATE TABLE IF NOT EXISTS `node` (
   `id` varchar(64) NOT NULL,
   `name` varchar(64) NOT NULL,
-  `domain_id` varchar(64) NOT NULL
+  `domain_id` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session`
+--
+
+CREATE TABLE IF NOT EXISTS `session` (
+  `id` varchar(64) NOT NULL,
+  `user_id` varchar(64) DEFAULT NULL,
+  `service_graph_id` varchar(64) NOT NULL,
+  `service_graph_name` varchar(64) NOT NULL,
+  `ingress_node` varchar(64) DEFAULT NULL,
+  `egress_node` varchar(64) DEFAULT NULL,
+  `status` varchar(64) NOT NULL,
+  `started_at` datetime DEFAULT NULL,
+  `last_update` datetime DEFAULT NULL,
+  `error` datetime DEFAULT NULL,
+  `ended` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `tenant_id` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `mail` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
+  `service_graph` text NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenant`
+--
+
+CREATE TABLE IF NOT EXISTS `tenant` (
+  `id` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `description` varchar(128) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 --
 -- Dumping data for table `node`
 --
 
-INSERT INTO `node` (`id`, `name`, `domain_id`) VALUES
+REPLACE INTO `user` (`id`, `name`, `password`, `tenant_id`) VALUES
+('0', 'user1', 'password1', 'public');
+
+--
+-- Dumping data for table `user`
+--
+
+REPLACE INTO `node` (`id`, `name`, `domain_id`) VALUES
 ('0', 'node0', '130.192.225.105'),
 ('1', 'node1', '130.192.225.193'),
 ('2', 'node2', '10.0.0.3'),
@@ -38,12 +97,10 @@ INSERT INTO `node` (`id`, `name`, `domain_id`) VALUES
 ('4', 'node4', '10.0.0.5');
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `tenant`
 --
 
---
--- Indexes for table `node`
---
-ALTER TABLE `node`
-  ADD PRIMARY KEY (`id`);
+REPLACE INTO `tenant` (`id`, `name`, `description`) VALUES
+('public', 'public', 'public tenant');
+
 
