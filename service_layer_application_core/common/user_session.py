f1cf372149f63430ce98cee77a6d27aeb98f2715
Gabriele Castellano <gabry.c92x@gmail.com>
2016-03-14 18:20:04 +0100

    Fixed a bug in checkSession where if the user haven't an active session the function raise an exception instead of return false

--- service_layer_application_core/common/user_session.py
+++ service_layer_application_core/common/user_session.py
@@ -5,6 +5,9 @@ Created on Feb 15, 2015
 '''
 import json
 import logging
+
+from requests import HTTPError
+
 from service_layer_application_core.sql.session import Session
 from service_layer_application_core.config import Configuration
 
@@ -25,15 +28,20 @@ class UserSession(object):
         if session_status is True:
             user_session = Session().get_active_user_session(self.user_id)
             if DEBUG_MODE is False:
-                response = json.loads(orchestrator.getNFFGStatus(nffg_id))
+                try:
+                    response = json.loads(orchestrator.getNFFGStatus(nffg_id))
+                except HTTPError as err:
+                    if err.response.status_code == 404:
+                        logging.debug("NFFG: "+str(nffg_id)+" not yet instantiated!")
+                        return False
                 if response['status'] != 'error' or response['status'] != 'not_found':
                     # The user profile is really instantiated
                     return True
-                if response['status'] != 'in_progress':
-                    logging.debug("NFFG: "+str(nffg_id)+" in progress!");
+                if response['status'] == 'in_progress':
+                    logging.debug("NFFG: "+str(nffg_id)+" in progress!")
                 else:
                     # Set error state for all active session for the user
-                    logging.debug("ERROR in NF-FG for user "+user_session.user_id);
+                    logging.debug("ERROR in NF-FG for user "+user_session.user_id)
                     Session().set_error_by_nffg_id(nffg_id)
             else:
                 return True
