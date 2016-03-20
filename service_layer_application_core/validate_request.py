15de76d8f772835b8a87adb722303d3833a4cbcf
Gabriele Castellano <gabry.c92x@gmail.com>
2016-03-20 11:32:52 +0100

    Fixed warnings and typos

--- service_layer_application_core/validate_request.py
+++ service_layer_application_core/validate_request.py
@@ -1,20 +1,39 @@
-'''
+"""
 Created on Oct 1, 2014
 
 @author: fabiomignini
-'''
+"""
 import logging
 
 from service_layer_application_core.exception import RequestValidationError
 from json_hyper_schema import Schema, ValidationError
 
-class RequestValidator():
-    
-    def validate(self, nffg):
-        schema={"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"session":{"type":"object","properties":{"mac":{"type":"string"}},"additionalProperties":False}},"required":["session"],"additionalProperties":False}
-        hyperschema = Schema(schema)
+
+class RequestValidator:
+    @staticmethod
+    def validate(nffg):
+        schema = {
+            "$schema": "http://json-schema.org/draft-04/schema#",
+            "type": "object",
+            "properties": {
+                "session": {
+                    "type": "object",
+                    "properties": {
+                        "mac": {
+                            "type": "string"
+                        }
+                    },
+                    "additionalProperties": False
+                }
+            },
+            "required": [
+                "session"
+            ],
+            "additionalProperties": False
+        }
+        hyper_schema = Schema(schema)
         try:
-            hyperschema.validate(nffg)
+            hyper_schema.validate(nffg)
         except ValidationError as err:
             logging.info(err.message)
             raise RequestValidationError(err.message)
