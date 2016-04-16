59c6af249050587640601119b8be440d481433a1
Gabriele Castellano <gabry.c92x@gmail.com>
2016-04-16 20:55:17 +0200

    added characterization option for end-points of type gre-tunnel

--- service_layer_application_core/common/endpoint.py
+++ service_layer_application_core/common/endpoint.py
@@ -4,20 +4,26 @@ Created on Feb 15, 2015
 @author: fabiomignini
 """
 import logging, json
+
+from service_layer_application_core.exception import GreIdNotFound
 from service_layer_application_core.nffg_manager import NFFG_Manager
 from service_layer_application_core.config import Configuration
+from service_layer_application_core.sql.domains_info import DomainInformation
 from service_layer_application_core.sql.node import Node
 
 ISP_INGRESS = Configuration().ISP_INGRESS
 ISP_EGRESS = Configuration().ISP_EGRESS
 CONTROL_INGRESS = Configuration().CONTROL_INGRESS
 USER_INGRESS = Configuration().USER_INGRESS
+REMOTE_USER_INGRESS = Configuration().REMOTE_USER_INGRESS
 CONTROL_EGRESS = Configuration().CONTROL_EGRESS
 
 INGRESS_PORT = Configuration().INGRESS_PORT
 INGRESS_TYPE = Configuration().INGRESS_TYPE
 EGRESS_PORT = Configuration().EGRESS_PORT
 EGRESS_TYPE = Configuration().EGRESS_TYPE
+REMOTE_INGRESS_TYPE = Configuration().REMOTE_INGRESS_TYPE
+REMOTE_INGRESS_PORT = Configuration().REMOTE_INGRESS_PORT
 
 ISP = Configuration().ISP
 
@@ -25,14 +31,14 @@ ISP = Configuration().ISP
 class Endpoint(object):
     def __init__(self, nffg):
         self.nffg = nffg
-    
+
     def connectEndpointSwitchToVNFs(self):
         manage = NFFG_Manager(self.nffg)
-            
+
         for endpoint in self.nffg.end_points:
             if endpoint.name == CONTROL_INGRESS or endpoint.name == ISP_INGRESS:
                 manage.connectEndpointSwitchToVNF(endpoint)
-                
+
     def characterizeEndpoint(self, user_id=None):
         # Characterize INGRESS endpoint
         for endpoint in self.nffg.end_points:
@@ -52,6 +58,19 @@ class Endpoint(object):
                 endpoint.interface = INGRESS_PORT
                 if user_id is not None:
                     endpoint.node = Node().getNodeDomainID(Node().getUserLocation(user_id))
+            elif endpoint.name == REMOTE_USER_INGRESS:
+                endpoint.type = REMOTE_INGRESS_TYPE
+                endpoint.interface = REMOTE_INGRESS_PORT
+                gre_model = DomainInformation.get_domain_gre(endpoint.id)
+                if gre_model is None:
+                    raise GreIdNotFound(
+                        "The end point '" + endpoint.id + "' of type gre have not an entry prepared in the database"
+                    )
+                endpoint.local_ip = gre_model.local_ip
+                endpoint.remote_ip = gre_model.remote_ip
+                endpoint.gre_key = gre_model.gre_key
+                if user_id is not None:
+                    endpoint.node = Node().getNodeDomainID(Node().getUserLocation(user_id))
             else:
                 endpoint.type = 'internal'
-            logging.debug("End-point characterized: "+json.dumps(endpoint.getDict()))
+            logging.debug("End-point characterized: " + json.dumps(endpoint.getDict()))
