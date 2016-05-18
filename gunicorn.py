8223459abc1201663baef4bfb8c2d9b115b31cae
Gabriele Castellano <gabry.c92x@gmail.com>
2016-05-18 11:49:28 +0200

    Update gunicorn.py
    Added configuration file specification

--- gunicorn.py
+++ gunicorn.py
@@ -1,6 +1,21 @@
 from subprocess import call
 from service_layer_application_core.config import Configuration
 
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+      '-d',
+      "--conf-file",
+      nargs='?',
+      help='Configuration file'
+)
+
+args = parser.parse_args()
+
+# set configuration file
+if args.file:
+  Configuration.config_file = args.file
+
 conf = Configuration()
 ip = conf.SERVICE_LAYER_IP
 port = conf.SERVICE_LAYER_PORT
